dc848b006821d8003fac88e9c0760d2854116cb5
Calvin Gonzalez <calvin.gonzalez@oddball.io>
2023-08-18 12:41:40 -0400

    Remove React Router

--- src/components/OAuthCcgAppInfo.tsx
+++ src/components/OAuthCcgAppInfo.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
 import TextField from './forms/TextField';
 import classNames from 'classnames';
 
@@ -9,17 +8,18 @@ interface OAuthCcgAppInfoProps {
 }
 
 const OAuthCcgAppInfo: React.FC<OAuthCcgAppInfoProps> = ({ ccgPublicKeyUrl, multipleTypes }): JSX.Element => (
-  <div className={classNames('sandbox-access-form-oauth-details', {
-    'multiple-types': multipleTypes,
-  })}>
+  <div
+    className={classNames('sandbox-access-form-oauth-details', {
+      'multiple-types': multipleTypes,
+    })}
+  >
     <h3>Client Credentials Grant</h3>
     <div className="vads-u-margin-top--2">
       <p>
-        In order to access an API that uses OAuth 2.0 Client Credentials Grant, you must provide
-        your public key.{' '}
-        <Link to={ccgPublicKeyUrl} target="_blank">
+        In order to access an API that uses OAuth 2.0 Client Credentials Grant, you must provide your public key.{' '}
+        <a href={ccgPublicKeyUrl} rel="noreferrer" target="_blank">
           Learn how to generate a public key.
-        </Link>
+        </a>
       </p>
     </div>
 
@@ -35,7 +35,11 @@ const OAuthCcgAppInfo: React.FC<OAuthCcgAppInfoProps> = ({ ccgPublicKeyUrl, mult
       required
       className="vads-u-margin-top--2"
     />
-    <p><strong>Important:</strong> To get production access using client credentials grant, you must either work for the VA or have specific VA agreements in place. If you have questions, <a href="https://developer.va.gov/support/contact-us">Contact us</a>.</p>
+    <p>
+      <strong>Important:</strong> To get production access using client credentials grant, you must either work for the
+      VA or have specific VA agreements in place. If you have questions,{' '}
+      <a href="https://developer.va.gov/support/contact-us">Contact us</a>.
+    </p>
   </div>
 );
 
